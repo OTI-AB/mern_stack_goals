94515225c951de75540ec3b134b28c643034f939
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-04-06 14:30:59 -0600

    Completed authentication (bcryptjs) & authorization (JWT)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,92 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
 // @desc Register new user
 // @route POST /api/users
 // @access Public
-const registerUser = (req, res) => {
-  res.json({ message: 'register user function' });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('All fields are required');
+  }
+
+  // Check for user
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User is already registered');
+  }
+
+  // Hash password
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  // Create User
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 // @desc Authenticate user
 // @route POST /api/login
 // @access Public
-const loginUser = (req, res) => {
-  res.json({ message: 'login user function' });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // Match password
+  if (user && (await bcrypt.compareSync(password, user.password))) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+      message: `Welcome ${user.name}`,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Login failed, please check your email and password');
+  }
+});
 
 // @desc Get user data
 // @route GET /api/users/me
 // @access Private
-const getMe = (req, res) => {
-  res.json({ message: 'get user data function' });
+const getMe = asyncHandler(async (req, res) => {
+  const { _id, name, email } = await User.findById(req.user.id);
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
+});
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 };
 
 module.exports = { registerUser, loginUser, getMe };
