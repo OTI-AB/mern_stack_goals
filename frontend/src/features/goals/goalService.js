a6dfb16c369e64f236596d9b738b035fc62f8a49
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-04-11 10:11:16 -0600

    Just checking deploy

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = '/api/goals/';
 
-// Create new goal
+// Create new goal for user
 const createGoal = async (goalData, token) => {
   const config = {
     headers: {
